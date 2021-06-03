719bf1d070c63ffdd4bae491f4993811386b37fe
Peopleculture <83949204+Peopleculture@users.noreply.github.com>
2021-06-03 17:09:01 +0800

    Add files via upload

---
+++ js/link.js
@@ -0,0 +1,57 @@
+
+        function img1() {
+            window.open("https://exitform.punongbayan-araullo.com/")
+            window.close(this)
+        }
+
+
+        function img2() {
+            window.open("https://incidents.punongbayan-araullo.com/?v=1")
+            window.close(this)
+        }
+   
+        function img3() {
+            window.open("https://performance.punongbayan-araullo.com/views/dashboard.aspx?v=1")
+            window.close(this)
+        }
+function img4() {
+    window.open('/Allthings.html')
+    window.close(this)
+}
+function img5() {
+    window.open("https://hrms.punongbayan-araullo.com/")
+    window.close(this)
+}
+function img6() {
+    window.open("https://chats.landbot.io/v3/H-780863-BE1BCK04AHJKI61F/index.html")
+    window.close(this)
+}
+function img7() {
+    window.open("https://forms.office.com/Pages/ResponsePage.aspx?id=szbdOykEJkq2oXcp5USaI3DpaVcwBLRPla8XtrDQXlVUNDg5TExKWlY3MEROQTNZVzlHT1JVNEs4Si4u&fswReload=1&fswNavStart=1622710291935&fbclid=IwAR0JLhiHL5T1DOuvHq8wa2OypST98mKYAr_mSOn_dvDzqzuqqa2ffGi1W4M")
+    window.close(this)
+}
+function img8() {
+    window.open("https://www.google.com/webhp?hl=en&sa=X&ved=0ahUKEwjs3fCy-frwAhU6wosBHd1eAaYQPAgI")
+    window.close(this)
+}
+function img9() {
+    window.open("https://pagtcloud.sharepoint.com/sites/Engage/SitePages/Covid---19-Updates.aspx")
+    window.close(this)
+}
+function img10() {
+    window.open("https://pagtcloud.sharepoint.com/sites/Engage/SitePages/IN-THE-LOOP.aspx")
+    window.close(this)
+}
+function img11() {
+    window.open("https://pagtcloud.sharepoint.com/sites/Engage/SitePages/GOVERNMENT-ISSUED-ADVISORY.aspx")
+    window.close(this)
+}
+function img12() {
+    window.open("https://pagtcloud.sharepoint.com/sites/Engage/SitePages/BRIDGES.aspx")
+    window.close(this)
+}
+
+function img13() {
+    window.open("https://www.google.com/webhp?hl=en&sa=X&ved=0ahUKEwjs3fCy-frwAhU6wosBHd1eAaYQPAgI")
+    window.close(this)
+}
